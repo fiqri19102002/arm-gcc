99f466c03c894c7cd48d02dcf86a6b9e0007671e
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2022-11-25 18:16:36 +0700

    Import ELF GCC 12.2.0 stable

---
+++ lib/gcc/arm-eabi/12.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --enable-languages=c,c++ --target=arm-eabi --prefix=/drone/src/builder/arm-eabi --disable-nls --disable-shared --with-newlib --disable-multilib --disable-werror CFLAGS='-g0 -O3 -fstack-protector-strong' CXXFLAGS='-g0 -O3 -fstack-protector-strong'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" } };
